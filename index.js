470511290f0bae5cd31d3a83eed40469163d5eb0
Edvin CANDON <edvincandon@gmail.com>
2016-05-23 13:22:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -141,6 +141,7 @@ S3AdapterTiny.prototype.deleteFile = function(filename) {
       let params = {
         Key: this._bucketPrefix + filename
       };
+      console.log(params);
       this._s3Client.deleteObject(params, (err, data) =>{
         if(err !== null) {
           return reject(err);
